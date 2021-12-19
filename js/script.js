3f63418add4c4e9cb2c11d0bd5de8687e3dff446
Sourabh <76570939+aspire-srv@users.noreply.github.com>
2021-12-20 00:44:56 +0530

    initial

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// var btn = document.querySelector(".btn");
+
+// btn.addEventListener("click", function() {
+//     var btn = document.createElement("div"); 
+//     btn.appendChild("hello");
+//     document.body.appendChild(btn);
+// })
